9a68644a8dd92cdbb274e2b2301b9994ce523809
Rene Pollesch <rene.pollesch@gmx.de>
2013-05-21 20:33:13 +0200

    added syslog logging facility and example configuration

---
+++ src/Savvy/Component/Logger/Syslog.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Savvy\Component\Logger;
+
+use Savvy\Base as Base;
+
+/**
+ * Send logging output to syslog
+ *
+ * @package Savvy
+ * @subpackage Component\Logger
+ */
+class Syslog extends AbstractLogger
+{
+    /**
+     * Name prefixed to log messages
+     *
+     * @var string
+     */
+    protected $name = null;
+
+    /**
+     * Set name property
+     *
+     * @param string $name
+     * @return \Savvy\Component\Logger\Syslog
+     */
+    public function setName($name)
+    {
+        $this->name = (string)$name;
+        return $this;
+    }
+
+    /**
+     * Get ame property
+     *
+     * @return string
+     */
+    protected function getName()
+    {
+        if ($this->name === null) {
+            $this->name = Base\Registry::getInstance()->get('name');
+        }
+
+        return $this->name;
+    }
+
+    /**
+     * Write line to logging facility
+     *
+     * @param string $msg a message to log
+     * @param int $priority level of importance
+     * @return bool true on success, or false on error
+     */
+    public function write($msg, $priority = LOG_INFO)
+    {
+        return syslog($priority, sprintf('%s %s', $this->getName(), $msg));
+    }
+}
