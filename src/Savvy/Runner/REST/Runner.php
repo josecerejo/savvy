53e83502c8d939e99bffba2006300311b2619862
Rene Pollesch <rene.pollesch@gmx.de>
2013-06-22 15:12:20 +0200

    initial commit of (yet empty) RESTful runner

---
+++ src/Savvy/Runner/REST/Runner.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Savvy\Runner\REST;
+
+/**
+ * Runner for RESTful services
+ *
+ * @package Savvy
+ * @subpackage Runner\REST
+ */
+class Runner extends \Savvy\Runner\AbstractRunner
+{
+    /**
+     * Returns true if this runner is suitable for current mode of operation
+     *
+     * @return bool
+     */
+    public function isSuitable()
+    {
+        return isset($_REQUEST['api']);
+    }
+
+    /**
+     * Send output to client
+     *
+     * @return int
+     */
+    public function run()
+    {
+        header('HTTP/1.1 401 Unauthorized', true, 401);
+        return 1;
+    }
+}
