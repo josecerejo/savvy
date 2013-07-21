6f5ca905a391def34db97212487b70fa31db650a
Rene Pollesch <rene.pollesch@gmx.de>
2013-07-21 21:55:35 +0200

    progress on REST API authentication

--- src/Savvy/Base/Session.php
+++ src/Savvy/Base/Session.php
@@ -26,7 +26,9 @@ class Session extends AbstractSingleton
     {
         @session_start();
 
-        if (isset($_SERVER['HTTP_APPLICATION_SESSION'])) {
+        if (isset($_REQUEST['api']) && isset($_SERVER['PHP_AUTH_PW'])) {
+            $this->setApplicationSessionId($_SERVER['PHP_AUTH_PW']);
+        } elseif (isset($_SERVER['HTTP_APPLICATION_SESSION'])) {
             $this->setApplicationSessionId($_SERVER['HTTP_APPLICATION_SESSION']);
         }
 
@@ -73,6 +75,15 @@ class Session extends AbstractSingleton
 
             if (is_array($applicationSession) && isset($applicationSession['username'])) {
                 $result = true;
+            }
+        } elseif (isset($_REQUEST['api'])) {
+            $em = Database::getInstance()->getEntityManager();
+            $sessions = $em->getRepository('Savvy\Storage\Model\Session');
+
+            if ($session = $sessions->findOneByApplicationSessionId($this->getApplicationSessionId())) {
+                if ($session->getUser()->getUsername() === $_SERVER['PHP_AUTH_USER']) {
+                    $result = true;
+                }
             }
         }
 
